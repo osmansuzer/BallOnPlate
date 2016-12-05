e6c73cb56e778aeb5e812f228e14a4a21a6a54ef
bekircanagaoglu <bagaoglu@gtu.edu.tr>
2016-12-05 17:11:39 +0300

    sdf

---
+++ main.py
@@ -0,0 +1,65 @@
+"""
+Emulate an oscilloscope.  Requires the animation API introduced in
+matplotlib 1.0 SVN.
+"""
+import serial
+import numpy as np
+from matplotlib.lines import Line2D
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+class Scope(object):
+    def __init__(self, ax, maxt=2, dt=0.02):
+        self.ax = ax
+        self.dt = dt
+        self.maxt = maxt
+        self.tdata = [0]
+        self.ydata = [0]
+        self.line = Line2D(self.tdata, self.ydata)
+        self.ax.add_line(self.line)
+        self.ax.set_ylim(-600, 600)
+        self.ax.set_xlim(0, self.maxt)
+
+    def update(self, y):
+        lastt = self.tdata[-1]
+        if lastt > self.tdata[0] + self.maxt:  # reset the arrays
+            self.tdata = [self.tdata[-1]]
+            self.ydata = [self.ydata[-1]]
+            self.ax.set_xlim(self.tdata[0], self.tdata[0] + self.maxt)
+            self.ax.figure.canvas.draw()
+
+        t = self.tdata[-1] + self.dt
+        self.tdata.append(t)
+        self.ydata.append(y)
+        self.line.set_data(self.tdata, self.ydata)
+        return self.line,
+
+ser = serial.Serial("/dev/ttyACM0", 9600) #open serial
+print('serial ok')
+def emitter():
+    while True:
+        x = int(ser.readline())
+        if(x < 600 and x > -600):
+            yield x
+        else:
+             yield 600
+
+def emitter1():
+    while True:
+        y = int(ser.readline())
+        if (y < 600 and y > -600):
+            yield y
+        else:
+            yield 600
+fig, ax = plt.subplots()
+fig1, ax1 = plt.subplots()
+scope = Scope(ax)
+scope1 = Scope(ax1)
+
+# pass a generator in "emitter" to produce data for the update func
+ani = animation.FuncAnimation(fig, scope.update, emitter, interval=1,
+                              blit=True)
+ani1 = animation.FuncAnimation(fig1, scope1.update, emitter1, interval=1,
+                              blit=True)
+
+plt.show()
